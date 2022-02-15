0f7efb4d3978f1a55f569788ea8cfe03bf9aa7e3
DonSteve <aabad01@ucm.es>
2022-02-15 13:04:28 +0100

    commit de prueba

--- server/src/keys.ts
+++ server/src/keys.ts
@@ -8,10 +8,3 @@ export default{
         port: 3306
     }
  }
-
-//  database: {
-//     host: 'us-cdbr-east-05.cleardb.net',
-//     user: 'bb95840969b521',
-//     password: '3600141e',
-//     database: 'heroku_16655e4b38bc751'
-// }
